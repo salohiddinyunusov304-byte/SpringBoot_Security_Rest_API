9b4c291922b91aa120aebeb8298e61557bc4275b
Salohiddin Yunusov <salohiddinyunusov304@gmail.com>
2026-03-02 17:00:13 +0500

    springboot

---
+++ src/main/java/uz/pdp/springboot_security_rest_api/SpringbootSecurityRestApiApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.springboot_security_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecurityRestApiApplication.class, args);
+    }
+
+}
